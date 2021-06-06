09ca516e50b9fc9b79b2d06c306fcadd287d6bf0
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-06 01:17:50 -0400

    adiconando metodos api de criar e listar

---
+++ src/controller/UserController.js
@@ -0,0 +1,24 @@
+
+const User = require('../model/user');
+
+module.exports = {
+    async list(req, res){
+        try{
+            const user = await User.findAll();
+            return res.json(user);
+        } catch (err){
+            return console.log("erro ao listar imovel");
+        }   
+
+},
+
+    async create(req, res){
+        const {nome, cpf,email,password }  = req.body;
+        try{
+            const user = await User.create({nome, cpf,email,password });
+            return res.json(user);
+        }catch (error){
+            return console.log("erro na criacao imovel",err);
+        }
+    }
+}
