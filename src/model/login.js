531746cef201ad1ace03c9e3ded1cd891892fcac
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-07 01:02:28 -0400

    adcionando bcrypt senha do login

--- src/model/login.js
+++ src/model/login.js
@@ -1,17 +1,28 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../database');
-
+const bcrypt = require("bcrypt");
 
 const schema = 'login';
 
 class Login extends Sequelize.Model {}
 Login.init({
     email: Sequelize.STRING,
-    password: Sequelize.STRING
+    senha: Sequelize.STRING,
+    
+    },{
+        hooks: {
+            beforeCreate: (login) => {
+              const salt = bcrypt.genSaltSync();
+   
+              login.senha = bcrypt.hashSync(login.senha,salt);
+            }
+          },
+          instanceMethods: {
+            validPassword: function(senha) {
+              return bcrypt.compareSync(senha, this.senha);
+            }
+          },sequelize, modelName: 'login', schema});
 
-}, {sequelize, modelName: 'login', schema});
-
-
-sequelize.sync();
+    sequelize.sync();
 
 module.exports = Login;
