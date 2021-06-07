57f6c6df557907a318fc8be045f6aa4a5d6809ca
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-07 18:57:09 -0400

    arrumando rota do update

--- src/routes.ts
+++ src/routes.ts
@@ -8,17 +8,17 @@ const routes = express.Router();
 
 
 //rotas do imove
-routes.get('/imo', ImovelController.list);
-routes.get('/imo/:id', ImovelController.show);
-routes.post('/imo', ImovelController.create);
-routes.put('/imo:id', ImovelController.update);
-routes.delete('/imo/:id', ImovelController.delete);
+routes.get('/imovel', ImovelController.list);
+routes.get('/imovel/:id', ImovelController.show);
+routes.post('/imovel', ImovelController.create);
+routes.put('/imovel/id', ImovelController.update);
+routes.delete('/imovel/:id', ImovelController.delete);
 
 //rotas users
 routes.get('/users', UserController.list);
 routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.create);
-routes.put('/users:id', UserController.update);
+routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
 
@@ -28,7 +28,7 @@ routes.delete('/users/:id', UserController.delete);
 routes.get('/login', LoginController.list);
 routes.get('/login/:id', LoginController.show);
 routes.post('/login', LoginController.create);
-routes.put('/login:id', LoginController.update);
+routes.put('/login/:id', LoginController.update);
 routes.delete('/login/:id', LoginController.delete);
 
 
