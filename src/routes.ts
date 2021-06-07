3496d7de08d3ac055dcae0d3450fa8e36e657f7a
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-06 23:35:06 -0400

    definindo rotas com as operacoes da api

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,24 @@
 import express = require('express');
 
 const ImovelController = require('./controller/ImovelController');
-
+const UserController = require('./controller/UserController');
 
 const routes = express.Router();
 
 
 //rotas do imove
-routes.post('/imo', ImovelController.create);
 routes.get('/imo', ImovelController.list);
-routes.gut('/imo:id', ImovelController.update);
+routes.get('/imo/:id', ImovelController.show);
+routes.post('/imo', ImovelController.create);
+routes.put('/imo:id', ImovelController.update);
+routes.delete('/imo/:id', ImovelController.delete);
 
+//rotas users
+routes.get('/users', UserController.list);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.create);
+routes.put('/users:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 
 
