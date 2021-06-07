1cdbf380d8f9941fd722728261e28d5a877a10a0
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-06 21:23:53 -0400

    metodos da api, list, create, show, update e delete criado

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -20,5 +20,44 @@ module.exports = {
         }catch (error){
             return console.log("erro na criacao user",err);
         }
+    },
+
+    async show(req, res){
+        try{
+            const user = await User.findAll({where: {id: req.params.id}});
+            return res.json(user);
+        } catch (err){
+            return console.logo("erro na pesquisa usuario", err);
+        }
+    },
+
+
+    async update(req, res){
+        const sequelize = require('sequelize');
+        const up = sequelize.up;
+        const {nome, cpf,email,password } = req.body;
+        const id = req.body;
+        
+        try{
+            await User.update({nome, cpf,email,password }, {where:{ id: {[up.eq]: id}}});
+            return res.json({msg: `Usuario  ${nome} atualizado com sucesso..`});
+            
+        } catch (error) {
+            return res.json({msg: `Usuario ${nome} não atualizado`}, err);            
+        }
+
+
+    },
+
+
+    async delete( req, res){
+        try{
+            await User.destroy({where: {id: req.params.id}});
+            return res.json({msg: `exclusao do usuario ${req.params.id} com sucesso`})
+
+        } catch (err) {
+            return console.err("Erro na exclusão: ", err);
+        }
     }
 }
+   
